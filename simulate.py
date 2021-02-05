048389e4f741f2830733a05556bf89eef0048ff7
cdavis11 <cdavis11@uvm.edu>
2021-02-05 11:24:12 -0500

    Removed box part

--- simulate.py
+++ simulate.py
@@ -7,10 +7,7 @@ import time as t
 # Display GUI
 physicsClient = p.connect(p.GUI)
 
-# Read in box
-p.loadSDF("box.sdf")
-
-# Create loop to make GUI visibe for ~16 seconds
+# Create loop to make GUI visible for ~16 seconds
 for i in range (0, 1000):
     p.stepSimulation()
     t.sleep(1/60)
