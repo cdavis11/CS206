e5b66ab9536ce58f0457f663d2ff16bc25dbf9b4
cdavis11 <78046807+cdavis11@users.noreply.github.com>
2021-02-04 20:19:56 -0500

    Add files via upload

---
+++ simulate.py
@@ -0,0 +1,16 @@
+# Created by Claire Davis
+# CS 206 Assingment 1
+
+import pybullet as p
+import time as t
+
+# Display GUI
+physicsClient = p.connect(p.GUI)
+
+# Create loop to make GUI visibe for ~16 seconds
+for i in range (0, 1000):
+    p.stepSimulation()
+    t.sleep(1/60)
+    
+# Close GUI
+p.disconnect()
