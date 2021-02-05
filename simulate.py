cbeeaf0c771282bf145a67df2ce30b0a3dceecee
cdavis11 <78046807+cdavis11@users.noreply.github.com>
2021-02-04 20:30:47 -0500

    Update simulate.py
    Add box

--- simulate.py
+++ simulate.py
@@ -7,6 +7,9 @@ import time as t
 # Display GUI
 physicsClient = p.connect(p.GUI)
 
+# Read in box
+p.loadSDF("box.sdf")
+
 # Create loop to make GUI visibe for ~16 seconds
 for i in range (0, 1000):
     p.stepSimulation()
