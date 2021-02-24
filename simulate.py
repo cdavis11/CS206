986d2b6644bd9aa3d97a60a2a9cedf41181a777e
cdavis11 <cdavis11@uvm.edu>
2021-02-23 22:29:38 -0500

    Robot stands on tiptoes

--- simulate.py
+++ simulate.py
@@ -6,7 +6,7 @@ import time as t
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy as np
-import os
+import math
 
 # Display GUI
 physicsClient = p.connect(p.GUI)
@@ -20,7 +20,7 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 
 # Simulate robot
-bodyId = p.loadURDF("body.urdf")
+robot = p.loadURDF("body.urdf")
 
 # Import box
 p.loadSDF("world.sdf")
@@ -28,16 +28,21 @@ p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate("body.urdf")
 
 # Create vectors filled with 0s
-backLegSensorValues = np.zeros(100)
-frontLegSensorValues = np.zeros(100)
+backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
 
 # Create loop to make GUI visibe for ~16 seconds
-for i in range (0, 100):
+for i in range (0, 1000):
     p.stepSimulation()
     # Add sensor to back and front leg
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    t.sleep(1/60)
+    # Simulate motor
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robot, jointName = "Torso_BackLeg", controlMode = p.POSITION_CONTROL,
+                                targetPosition = -math.pi/6.0, maxForce = 500)
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robot, jointName = "Torso_FrontLeg", controlMode = p.POSITION_CONTROL,
+                                targetPosition = math.pi/6.0, maxForce = 500)
+    t.sleep(1/100)
 
 # Save sensor values
 np.save('data/backLegSensorValues.npy', backLegSensorValues)
